adce0cc5f4fb1d2835dd80127f311b11de5fd8c1
Andres Lamilla <aflamillac@gmail.com>
2025-12-06 13:43:07 +0100

    AOC 2025 day2 part2

--- aoc2025/ocaml/lib/day2.ml
+++ aoc2025/ocaml/lib/day2.ml
@@ -17,7 +17,7 @@ module IDRange = struct
   let seq (idr : t) : int Seq.t =
     Seq.ints (fst idr) |> Seq.take (snd idr - fst idr + 1)
 
-  let is_invalid_id (id : int) : bool =
+  let is_invalid_id_v1 (id : int) : bool =
     (* you can find the invalid IDs by looking for any ID which is made only of
      some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice),
      and 123123 (123 twice) would all be invalid IDs *)
@@ -31,9 +31,35 @@ module IDRange = struct
       if d1 = d2 then true else false
     else false
 
-  let find_invalid_ids (idr : t) : int list =
+  let split_id (id : string) (partition : int) : string list =
+    let num_digits = String.length id in
+    let part_size = num_digits / partition in
+    List.init partition (fun i ->
+        let start = i * part_size in
+        let end_ = start + part_size in
+        String.sub id start (end_ - start))
+
+  let is_repeated (id : string) (partition : int) : bool =
+    let num_digits = String.length id in
+    if num_digits mod partition = 0 then
+      let id_splits = split_id id partition in
+      let first_split = List.hd id_splits in
+      List.for_all (fun s -> s = first_split) id_splits
+    else false
+
+  let is_invalid_id_v2 (id : int) : bool =
+    (* Now, an ID is invalid if it is made only of some sequence of digits
+       repeated at least twice. So, 12341234 (1234 two times), 123123123
+       (123 three times), 1212121212 (12 five times), and 1111111
+       (1 seven times) are all invalid IDs *)
+    let str_id = Int.to_string id in
+    let num_digits = String.length str_id in
+    let partitions = Seq.ints 2 |> Seq.take (num_digits - 1) |> List.of_seq in
+    List.exists (is_repeated str_id) partitions
+
+  let find_invalid_ids fn (idr : t) : int list =
     let s = seq idr in
-    Seq.filter is_invalid_id s |> List.of_seq
+    Seq.filter fn s |> List.of_seq
 end
 
 let parse_line (s : string option) (acc : IDRange.t list) : IDRange.t list =
@@ -44,8 +70,14 @@ let parse_line (s : string option) (acc : IDRange.t list) : IDRange.t list =
 
 let part1 () =
   let id_ranges = Util.read_lines file parse_line in
-  let invalid_ids =
-    List.map IDRange.find_invalid_ids id_ranges |> List.flatten
-  in
+  let find_invalid_ids = IDRange.find_invalid_ids IDRange.is_invalid_id_v1 in
+  let invalid_ids = List.map find_invalid_ids id_ranges |> List.flatten in
+  let sum_invalid_ids = List.fold_left ( + ) 0 invalid_ids in
+  Printf.printf "Invalid ids: (%d)\n" sum_invalid_ids
+
+let part2 () =
+  let id_ranges = Util.read_lines file parse_line in
+  let find_invalid_ids = IDRange.find_invalid_ids IDRange.is_invalid_id_v2 in
+  let invalid_ids = List.map find_invalid_ids id_ranges |> List.flatten in
   let sum_invalid_ids = List.fold_left ( + ) 0 invalid_ids in
   Printf.printf "Invalid ids: (%d)\n" sum_invalid_ids
